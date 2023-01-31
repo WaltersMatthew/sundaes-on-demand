2d8e55404ff0de378835b8e721c8f37517da32e3
waltersmatthew <waltersmatthew88@gmail.com>
2023-01-30 16:22:20 -0800

    code and test topping options

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
+import Options from "./pages/entry/Options";
 import SummaryForm from "./pages/summary/SummaryForm";
 
 function App() {
-    return <SummaryForm />;
+    return <Options optionType={"scoops"} />;
 }
 
 export default App;
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -13,3 +13,19 @@ test("displays image for each scoop option from the server", async () => {
     const altText = scoopImages.map((element) => element.alt);
     expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("displays image for each topping option from the server", async () => {
+    render(<Options optionType="toppings" />);
+
+    const toppingImages = await screen.findAllByRole("img", {
+        name: /topping$/i,
+    });
+    expect(toppingImages).toHaveLength(3);
+
+    const altText = toppingImages.map((element) => element.alt);
+    expect(altText).toEqual([
+        "Cherries topping",
+        "M&Ms topping",
+        "Hot fudge topping",
+    ]);
+});
