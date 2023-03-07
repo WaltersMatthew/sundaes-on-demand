f36c863036ed057ff4fc64ce7cb559789857a69b
waltersmatthew <waltersmatthew88@gmail.com>
2023-03-07 12:51:30 -0800

    add loading conditional render to pass tests

--- src/pages/Confirmation/OrderConfirmation.jsx
+++ src/pages/Confirmation/OrderConfirmation.jsx
@@ -1,36 +1,39 @@
-import Button from "react-bootstrap/Button";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { useOrderDetails } from "../../contexts/OrderDetails";
+import Button from "react-bootstrap/Button"
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { useOrderDetails } from "../../contexts/OrderDetails"
 
 export default function OrderConfirmation({ setOrderPhase }) {
-    const { resetOrder } = useOrderDetails();
-    const [orderNumber, setOrderNumber] = useState(null);
+    const { resetOrder } = useOrderDetails()
+    const [orderNumber, setOrderNumber] = useState(null)
 
     useEffect(() => {
         axios
             .post(`http://localhost:3030/order`)
             .then((response) => {
-                setOrderNumber(response.data.orderNumber);
+                setOrderNumber(response.data.orderNumber)
             })
             .catch((error) => {
                 //TODO: handle error here
-            });
-    }, []);
+            })
+    }, [])
 
     function handleClick() {
         //clear the order
-        resetOrder();
+        resetOrder()
         //send back to order page
-        setOrderPhase("inProgress");
+        setOrderPhase("inProgress")
     }
-
-    return (
-        <div style={{ textAlign: "center" }}>
-            <h1>Thank You!</h1>
-            <h2>Your order number is {orderNumber}</h2>
-            <p>as per our terms and conditions, nothing will happen here</p>
-            <Button onClick={handleClick}>Create New Order</Button>
-        </div>
-    );
+    if (orderNumber) {
+        return (
+            <div style={{ textAlign: "center" }}>
+                <h1>Thank You!</h1>
+                <h2>Your order number is {orderNumber}</h2>
+                <p>as per our terms and conditions, nothing will happen here</p>
+                <Button onClick={handleClick}>Create New Order</Button>
+            </div>
+        )
+    } else {
+        return <div>loading</div>
+    }
 }
